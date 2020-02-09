96b3a64facb66bc913be7c8bb733fed58dc26cc1
Zainab Waleed <ZainabW@Air.local>
2020-02-09 16:37:37 +0100

    om.html

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("Siden er loadet");
 }
+
+
+$(document).ready(function () {
+    $("button").click(function () {
+        $("ul").toggle(500);
+    });
+});
